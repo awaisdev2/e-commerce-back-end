ece127227b08544530cfeff180c7e989ee9dd771
Awais <awaisdev2@gmail.com>
2023-09-24 14:22:59 +0500

    incomplete project

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.post('/register', async function(req, res) {
     let user = new User(req.body);
     let result = await user.save()
-    res.send(req.body);
+    res.send(result);
 });
 
 app.listen(4500, () => {
